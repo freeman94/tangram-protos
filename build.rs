f0ba690d2d9e63c34d3e94612342a7ecf8aa9696
Jacob Freeman <jacob@saronic.com>
2026-03-10 15:03:42 -0500

    chore: bump to latest

--- build.rs
+++ build.rs
@@ -25,6 +25,27 @@ fn main() {
         .collect();
     protos.extend(tangram_protos);
 
+    // Add tangram detection protos
+    let detections_pattern = "tangram-protobuf-messages/protos/tangram/detections/*.proto";
+    let detections_protos: Vec<PathBuf> = glob(detections_pattern).unwrap()
+        .filter_map(|e| match e {
+            Ok(v) => Some(v),
+            Err(_) => None,
+        })
+        .collect();
+    protos.extend(detections_protos);
+
+    // Add ROS protos if the ros feature is enabled
+    if cfg!(feature = "ros") {
+        let ros_protos: Vec<PathBuf> = glob("tangram-protobuf-messages/protos/ros/**/*.proto").unwrap()
+            .filter_map(|e| match e {
+                Ok(v) => Some(v),
+                Err(_) => None,
+            })
+            .collect();
+        protos.extend(ros_protos);
+    }
+
     Codegen::new()
         .pure()
         .cargo_out_dir("generated_protos")
